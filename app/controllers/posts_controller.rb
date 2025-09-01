3292e2ff755ac78d0b98172f4640068850c8f2f8
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-09-02 01:50:51 +0900

    feat:mdファイルの読み取り処理追加 #4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,21 @@
+require "front_matter_parser"
+
 class PostsController < ApplicationController
-  def index; end
+  BLOG_POSTS_DIR = Rails.root.join("app/blog_posts")
+
+  def index
+    md_files = Dir.glob(BLOG_POSTS_DIR.join("*.md")).sort.reverse
+    @posts = md_files.map do |file|
+      parsed = FrontMatterParser::Parser.parse_file(file)
+      {
+        filename: File.basename(file, ".md"),
+        title: parsed.front_matter["title"],
+        date: parsed.front_matter["date"],
+        date_hyphen: parsed.front_matter["date"].gsub("-", "/"),
+        summary: parsed.front_matter["summary"],
+      }
+    end
+  end
 
   def show; end
 end
