165c95b0acc3a2db88b81ca4de7911eedae82f8c
kumaa-n <kuma2557.guba1@gmail.com>
2025-09-02 17:49:59 +0900

    clean:Lintチェックエラーを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,14 @@ module ApplicationHelper
     links = {
       github: "https://github.com/kumaa-n",
       note:   "https://note.com/kumaa_n",
-      x:      "https://x.com/KUMAA_N_",
+      x:      "https://x.com/KUMAA_N_"
     }
 
     links[name]
   end
 
-  def page_title(title = '')
-    base_title = 'くまーんの巣'
+  def page_title(title = "")
+    base_title = "くまーんの巣"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
 end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -4,13 +4,13 @@ module ProfilesHelper
       {
         title: "Regexp? Quiz",
         desc: "正規表現の4択クイズができる学習用Webアプリです。　Sinatraで作成しました！",
-        url: "https://regexpquiz.onrender.com/",
+        url: "https://regexpquiz.onrender.com/"
       },
       {
         title: "pdf_tool",
         desc: "Pythonで作成したPDF用のデスクトップアプリです！　レイアウトがいまいちなので修正中…",
-        url: "https://github.com/kumaa-n/pdf_tool",
-      },
+        url: "https://github.com/kumaa-n/pdf_tool"
+      }
     ]
   end
 end
