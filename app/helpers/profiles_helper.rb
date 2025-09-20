da15d8ba4863a32d2d987cdc9064b1df02c00bfb
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2025-09-20 23:59:29 +0900

    feat:プロフィールページに新アプリを反映 #44

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -2,15 +2,20 @@ module ProfilesHelper
   def apps
     [
       {
-        title: "Regexp? Quiz",
-        desc: "正規表現の4択クイズができる学習用Webアプリです。　Sinatraで作成しました！",
-        url: "https://regexpquiz.onrender.com/"
-      },
-      {
         title: "pdf_tool",
         desc: "Pythonで作成したPDF用のデスクトップアプリです！　レイアウトがいまいちなので修正中…",
         url: "https://github.com/kumaa-n/pdf_tool"
-      }
+      },
+      {
+        title: "Regexp? Quiz",
+        desc: "正規表現の4択クイズができる学習用Webアプリです。　Sinatraで作成しました！",
+        url: "https://regexpquiz.onrender.com"
+      },
+      {
+        title: "秋フレーズジェネレーター",
+        desc: "秋の季節を感じられるフレーズをランダムに生成するWebアプリケーションです。",
+        url: "https://autumn-phrase-generator.vercel.app"
+      },
     ]
   end
 end
